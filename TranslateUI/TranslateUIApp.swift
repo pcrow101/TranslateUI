d9320f3dc82b07fbba0b6f19ae769edbe1f67f74
pcrow <pcrow@icloud.com>
2026-08-17 11:59:11 +0100

    Initial Commit

---
+++ TranslateUI/TranslateUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateUIApp.swift
+//  TranslateUI
+//
+//  Created by paucrow on 17/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct TranslateUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TranslateUITests/TranslateUITests.swift
@@ -0,0 +1,19 @@
+//
+//  TranslateUITests.swift
+//  TranslateUITests
+//
+//  Created by paucrow on 17/08/2026.
+//
+
+import Testing
+@testable import TranslateUI
+
+struct TranslateUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
